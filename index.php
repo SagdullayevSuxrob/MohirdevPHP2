c0068e714891440584ba675a2d56eb369156316c
Suxrob Sagdullayev <suxrobsagdullayev5@gmail.com>
2025-12-04 23:24:43 +0500

    Comments (izohlar)

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    echo "Assalomu alaykum";
+?>
+
+<h1 style="color: red;">Assalomu alaykum</h1>
+
+<?php
+    echo "Xayr";
+
+
+    echo " <h1>Hello world</h1>";
+?>
+<div>
+    <!--Short open tag -->
+    <?= "Bu div"?>
+</div>
+
+<?php
+    // Bu kommentariya 
+    echo "bu matn </br>";
+    // echo "bu matn";
+    echo "bu matn </br>";
+    # echo "bu matn"; 
+    echo "bu matn </br>";
+    /* echo "bu matn";
+    echo "bu matn";
+    echo "bu matn";*/
+    echo "bu matn </br>";
+    echo "bu matn";
+?>
